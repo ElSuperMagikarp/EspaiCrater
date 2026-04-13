dea071dda817ea4e4c327f02155e4d91cc499a19
ElSuperMagikarp <uriens2006@gmail.com>
2026-04-13 17:34:27 +0200

    first commit

---
+++ src/routes/(app)/page2/+page.svelte
@@ -0,0 +1,31 @@
+<main class="container bg-red-800">
+  <a href="/">Go to Page 1</a>
+  <h1>Page 2</h1>
+  <h1>Page 2</h1>
+  <h1>Page 2</h1>
+  <h1>Page 2</h1>
+  <h1>Page 2</h1>
+  <h1>Page 2</h1>
+  <h1>Page 2</h1>
+  <h1>Page 2</h1>
+  <h1>Page 2</h1>
+  <h1>Page 2</h1>
+  <h1>Page 2</h1>
+  <h1>Page 2</h1>
+  <h1>Page 2</h1>
+  <h1>Page 2</h1>
+  <h1>Page 2</h1>
+  <h1>Page 2</h1>
+  <h1>Page 2</h1>
+  <h1>Page 2</h1>
+  <h1>Page 2</h1>
+  <h1>Page 2</h1>
+  <h1>Page 2</h1>
+  <h1>Page 2</h1>
+  <h1>Page 2</h1>
+  <h1>Page 2</h1>
+  <h1>Page 2</h1>
+  <h1>Page 2</h1>
+  <h1>Page 2</h1>
+  <h1>Page 2</h1>
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<main class="size-full flex-center flex-col bg-blue-800">
+  <a href="/page2">Go to Page 2</a>
+  <h1 class="text-title text-2xl">Page 1</h1>
+</main>
