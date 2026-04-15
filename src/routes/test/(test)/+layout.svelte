f2878acc811da91ba5c7c8fba6b21a0ba5b089e1
ElSuperMagikarp <uriens2006@gmail.com>
2026-04-15 18:45:09 +0200

    Improved go back button

--- src/routes/test/(test)/+layout.svelte
+++
@@ -1,5 +0,0 @@
-<div class="absolute top-2 left-1">
-    <a href="/test" class="p-1 bg-white rounded-md">Go back</a>
-</div>
-
-<slot />
---
+++ src/routes/test/+layout.svelte
@@ -0,0 +1,10 @@
+<div class="absolute top-2 left-1">
+    <button
+        class="p-1 bg-white hover:bg-gray-400 rounded-md hover:cursor-pointer"
+        on:click={() => history.back()}
+    >
+        &lt; Go back
+    </button>
+</div>
+
+<slot />
