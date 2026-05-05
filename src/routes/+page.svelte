9db7aa1e041961da49b09fc209957c59dcb0bdfb
ElSuperMagikarp <uriens2006@gmail.com>
2026-05-05 16:05:49 +0200

    Added page "Inici de tot"

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,7 @@
 <main class="flex items-center py-10 flex-col bg-crater">
   <h1 class="text-title text-2xl">Benvingut a Espai Cràter. On vols anar?</h1>
-  <a href="/test" class="button">testing</a>
+  <div class="flex gap-2 pt-5">
+    <a href="/inici-de-tot" class="button">Inici de tot</a>
+    <a href="/test" class="button">testing</a>
+  </div>
 </main>
---
+++ src/routes/inici-de-tot/+page.svelte
@@ -0,0 +1,4 @@
+<main class="flex-center flex-col gap-y-4 bg-terra-2">
+    <h1 class="text-title text-2xl">INICI DE TOT</h1>
+    <button class="button"> Clicable </button>
+</main>
